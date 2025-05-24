0980e4e2257426fe2745766766c744ac4ed79bae
blumjn <blumjn@gmail.com>
2025-05-24 10:39:52 -0400

    Look there is a lot here, it is basically a new program

--- config.py
+++ config.py
@@ -1,2 +1,26 @@
-exposed_map = []
 
+#This file sets up the raith and is where the parameters such as pixel size and beamstep get set
+
+alpha = 16 #forward beam size in nm
+beta = 1500 #scattered beam size in nm
+eta = 0.5 #effectiveness of scattered beam in %
+
+pixel_size = 1 #length of 1 pixel in nm, must be <= beamstep
+beamstep_nm = 1 #beam step in nm, must be >= and divisible by pixel_size
+dose = 400 #dose in uC/cm^2, gets adjusted based on beamstep at start of run
+threshold = 175 #dose threshold for full development
+beamstep_px = [] #converts beamstep to be in terms of pixels instead of nm, set at start of run
+
+width = 200 #set this as width in nm, becomes width in pixels as integer, set at start of run
+height = 200 #set this as width in nm, becomes width in pixels as integer, set at start of run
+
+nt = 16 #number of threads allowed for multithreading of exposure
+
+## Shape defining parameters
+gap_height = 20
+gap_width = 16
+antenna_height = 60
+antenna_width = 70
+
+## plotting constants
+tickspacing = 25
---
+++ main_test.py
@@ -0,0 +1,129 @@
+#! /usr/bin/python3
+
+##--------------------------------------------------------------------\
+#   pso_python
+#   './pso_python/src/main_test.py'
+#   Test function/example for using the 'swarm' class in particle_swarm.py.
+#       This has been modified from the original to include message 
+#       passing back to the parent class or testbench, rather than printing
+#       error messages directly from the 'swarm' class. Format updates are 
+#       for integration in the AntennaCAT GUI.
+#
+#   Author(s): Jonathan Lundquist, Lauren Linkous
+#   Last update: March 12, 2025
+##--------------------------------------------------------------------\
+
+import pandas as pd
+import numpy as np
+from particle_swarm import swarm
+
+# OBJECTIVE FUNCTION SELECTION
+#import one_dim_x_test.configs_F as func_configs     # single objective, 1D input
+#import himmelblau.configs_F as func_configs         # single objective, 2D input
+import configs_F as func_configs     # multi objective function
+
+
+
+if __name__ == "__main__":
+    # Constant variables
+    NO_OF_PARTICLES = 11         # Number of particles in swarm
+    T_MOD = 0.65                 # Variable time-step extinction coefficient
+    TOL = 10 ** 1              # Convergence Tolerance
+    MAXIT = 10               # Maximum allowed iterations
+    BOUNDARY = 1                 # int boundary 1 = random,      2 = reflecting
+                                    #              3 = absorbing,   4 = invisible
+
+    # Objective function dependent variables
+    func_F = func_configs.OBJECTIVE_FUNC  # objective function
+    constr_F = func_configs.CONSTR_FUNC   # constraint function
+
+    LB = func_configs.LB              # Lower boundaries, [[0.21, 0, 0.1]]
+    UB = func_configs.UB              # Upper boundaries, [[1, 1, 0.5]]   
+    OUT_VARS = func_configs.OUT_VARS  # Number of output variables (y-values)
+    TARGETS = func_configs.TARGETS    # Target values for output
+
+    # target format. TARGETS = [0, ...] 
+
+    # threshold is same dims as TARGETS
+    # 0 = use target value as actual target. value should EQUAL target
+    # 1 = use as threshold. value should be LESS THAN OR EQUAL to target
+    # 2 = use as threshold. value should be GREATER THAN OR EQUAL to target
+    #DEFAULT THRESHOLD
+    #THRESHOLD = np.zeros_like(TARGETS) 
+    #THRESHOLD = np.ones_like(TARGETS)
+    THRESHOLD = [0]
+
+
+    # optimizer constants
+    WEIGHTS = [[0.5, 0.7, 0.78]]        # Update vector weights
+    VLIM = 5                         # Initial velocity limit
+
+
+    best_eval = 1e22
+    parent = None             # for the optimizer test ONLY
+    evaluate_threshold = True # use target or threshold. True = THRESHOLD, False = EXACT TARGET
+    suppress_output = True    # Suppress the console output of particle swarm
+    allow_update = True       # Allow objective call to update state 
+
+
+    # Constant variables in a list format
+    opt_params = {'NO_OF_PARTICLES': [NO_OF_PARTICLES], # Number of particles in swarm
+                'T_MOD': [T_MOD],                       # Variable time-step extinction coefficient
+                'BOUNDARY': [BOUNDARY],                 # int boundary 1 = random,      2 = reflecting
+                                                        #              3 = absorbing,   4 = invisible
+                'WEIGHTS': [WEIGHTS],                   # Update vector weights
+                'VLIM':  [VLIM] }     
+    # dataframe conversion
+    opt_df = pd.DataFrame(opt_params)
+
+    # optimizer initialization
+    myOptimizer = swarm(LB, UB, TARGETS, TOL, MAXIT,
+                            func_F, constr_F,
+                            opt_df,
+                            parent=parent, 
+                            evaluate_threshold=evaluate_threshold, obj_threshold=THRESHOLD)  
+
+    while not myOptimizer.complete():
+        # step through optimizer processing
+        # this will update particle or agent locations
+        myOptimizer.step(suppress_output)
+        # call the objective function, control 
+        # when it is allowed to update and return 
+        # control to optimizer
+        myOptimizer.call_objective(allow_update)
+        # check the current progress of the optimizer
+        # iter: the number of objective function calls
+        # eval: current 'best' evaluation of the optimizer
+        iter, eval = myOptimizer.get_convergence_data()
+        if (eval < best_eval) and (eval != 0):
+            best_eval = eval
+        
+        # optional. if the optimizer is not printing out detailed 
+        # reports, preview by checking the iteration and best evaluation
+
+        if suppress_output:
+            if iter%1 ==0: #print out every 100th iteration update
+                print("Iteration")
+                print(iter)
+                print("Best Eval")
+                print(best_eval)
+                print(myOptimizer.get_optimized_soln())
+                print(myOptimizer.get_optimized_outs())
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Optimized Solution")
+    print(myOptimizer.get_optimized_soln())
+    print("Optimized Outputs")
+    print(myOptimizer.get_optimized_outs())
+
+import time
+from swarm_feeder_v3 import *
+
+start_time=time.time()
+X = myOptimizer.get_optimized_soln()
+
+F, noErrors = PECS_feeder(X, 1)
+
+print("Runtime: ",(time.time()-start_time)," s")
+
+if 1:
+  plot_shapes()
